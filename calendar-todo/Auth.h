1a7408bc60d87fcb3e7e81d6419fea6bbfb489d1
ADiks <antondikson@gmail.com>
2020-07-28 15:04:44 +0300

    refactor

--- calendar-todo/Auth.h
+++ calendar-todo/Auth.h
@@ -7,54 +7,13 @@
 
 class Auth
 {
-	std::string name;
-	std::string password;
 	API api;
-
-	bool isValidation(std::string value)
-	{
-		return value.find_first_of(":*&/;,+-=") != std::string::npos ? true : false;
-	}
-
-	void warningUser(std::string data) 
-	{
-		std::cout << "Введенные вами данные: " << data << " не коректные, повторите заново" << std::endl;
-	}
-
 public:
-	void setUserName(std::string name) 
-	{
-		if (isValidation(name))
-			warningUser(name);
-		else
-			this->name = name; 
-	}
-
-	void setUserPassword(std::string password)
-	{
-		if (isValidation(password))
-			warningUser(password);
-		else
-			this->password = password;
-
-	}
-
 	bool login();
 	
 	bool registration();
 
 	json jsonData(std::string* mes);
 
-	std::string getUserName() const {
-		
-			return this->name;
-		
-	}
-	
-
-	std::string getUserPassword() const
-	{
-		return this->password;
-	}
 };
 
